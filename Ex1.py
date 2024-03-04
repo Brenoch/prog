ba7b7fc7858fddb70a2a5fc4ee9911aa0f8cbdc4
Brenoch <103227487+Brenoch@users.noreply.github.com>
2024-03-04 01:28:54 -0300

    readme

--- Ex1.py
+++ Ex1.py
@@ -1,9 +1,10 @@
+
 A = float(input("Informe o parâmetro a da equação: "))
 B = float(input("Informe o parâmetro b da equação: "))
 C = float(input("Informe o parâmetro c da equação: "))
 
 delta = B ** 2 - 4 * A * C
-x1 = (- B + delta**0.5)/(2*A) 
+x1 = (- B + delta**0.5)/(2*A)
 x2 = (- B - delta**0.5)/(2*A)
 
 print(x1)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # prog
-Repositório para estudo de linguagens de programação 
+Repositório para estudo de linguagens de programação
+
+
+Programação Estruturada
+AC1
+
+Breno Chaves da Cunha
+Matricula: 2024 0279 8502
