b35cfaac1b6cb77b97b838c46f5d36ff730ce1b0
Brenoch <103227487+Brenoch@users.noreply.github.com>
2024-03-04 01:20:48 -0300

    ex2
    exercício 2: anos bissextos

--- Ex1.py
+++ Ex1.py
@@ -3,8 +3,9 @@ B = float(input("Informe o parâmetro b da equação: "))
 C = float(input("Informe o parâmetro c da equação: "))
 
 delta = B ** 2 - 4 * A * C
-x1 = (- B + delta**0.5)/(2*A)
+x1 = (- B + delta**0.5)/(2*A) 
 x2 = (- B - delta**0.5)/(2*A)
 
 print(x1)
 print(x2)
+
---
+++ Ex2.py
@@ -0,0 +1,3 @@
+ano = int(input('Ano:'))
+
+print ((ano%4==0 and ano%100!=0) or (ano%400==0))
